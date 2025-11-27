ce78f4447840d9648ce21da6fce7bfc459d0bab4
舟木健児 <2009.k.funaki@gmail.com>
2025-11-27 23:47:15 +0900

    2025.11.27 auth.tsにtrustHostオプションを追加

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,5 @@
-// // middleware.ts
 // middleware.ts
 import { auth } from "@/auth"
 
 export { auth }   // Edge middleware 用
 export const config = { matcher: ["/courses/:path*", "/upload/:path*"] }
-
